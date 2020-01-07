6666bfcb2705e1fb358458810c712e404a666e2e
PrettyKittie <34041424+PrettyKittie@users.noreply.github.com>
2020-01-07 15:15:51 -0500

    Update kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -9,6 +9,6 @@ apt autoclean
 ##TODO convert this section to allow an option to add/remove tools from the list
 ##TODO add additional github tools, as needed (powersploit,empire,impacket,bloodhound,etc)
 #install additional tools
-apt-y install virtualbox vlc synaptic hexchat veil-evasion git
+apt -y install virtualbox vlc synaptic hexchat veil-evasion git
 wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
