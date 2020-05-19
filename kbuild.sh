783d38fd30198f87f2ecc54c80f1e4b78a26ee6d
PrettyKittie <34041424+PrettyKittie@users.noreply.github.com>
2020-05-18 20:31:56 -0400

    Update kbuild.sh

--- kbuild.sh
+++ kbuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 ##  ( X )    Fully update Kali             ##
 ##  (   )    Install necessary packages    ##
 ##  (   )    Download useful binaries      ##
@@ -9,15 +7,11 @@
 ##  (   )    Configure the environment     ##
 ##TODO add additional github tools, as needed (powersploit,empire,impacket,bloodhound,etc)
 
-
 ##  ( X )    Fully update Kali             ##
 apt update
 apt -y full-upgrade
-apt -y autoremove
-apt autoclean
 
 ##TODO convert this section to allow an option to add/remove tools from the list
-
 ##  (   )    Install necessary packages    ##
 #   ( X )         Virtualbox
 #   ( X )         VLC
@@ -49,6 +43,9 @@ apt autoclean
 #   (   )         Hot Proxy
 
 apt -y install virtualbox vlc synaptic hexchat veil-evasion git gparted firefox-esr chromium socat terminator gedit gobuster
+apt -y autoremove
+apt autoclean
+
 wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 chmod a+rx /usr/local/bin/youtube-dl
 
